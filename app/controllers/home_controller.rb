edd7c9f9b58b29c7b067c97ff5596a517c760153
Tushar Tuteja <ttuteja@gmail.com>
2017-02-04 16:32:32 +0530

    first commit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,22 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def evaluate
+    respond_to do |format|
+      format.js {
+        file = File.open('tmp/ruby.rb', 'w')
+        file.syswrite(params["code"])
+        file.close
+
+        system('ruby tmp/ruby.rb > tmp/result.txt')
+
+        @result = File.read('tmp/result.txt')
+        @code = params["code"]
+
+
+      }
+
+    end
+  end
 end
